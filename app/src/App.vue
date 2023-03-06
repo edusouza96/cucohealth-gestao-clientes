8634d82421c27b055329e07f569e3bef176faf2a
Eduardo Souza <eduardo.dasilva@canoastec.rs.gov.br>
2023-03-06 14:17:52 -0300

    APP - iniciando projeto

---
+++ app/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+    <div>
+        <header>
+            <div class="wrapper">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">Cuco Health</a>
+                        <button
+                            class="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarNavAltMarkup"
+                            aria-controls="navbarNavAltMarkup"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div
+                            class="collapse navbar-collapse"
+                            id="navbarNavAltMarkup"
+                        >
+                            <div class="navbar-nav">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+
+        <main class="">
+            <RouterView />
+        </main>
+    </div>
+</template>
